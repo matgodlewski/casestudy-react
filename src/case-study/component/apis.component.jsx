c8224ec01702c53934900bd818ae400e8b389c2b
Tim Hlebik <tsimafei.hlebik@ttpsc.pl>
2022-07-19 14:51:31 +0200

    refactoring

--- src/case-study/component/apis.component.jsx
+++ src/case-study/component/apis.component.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-class ApisComponent extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={ () => this.props.onClick() }>Get all apis</button>
-      </div>
+const ApisComponent = ({ onClick, apis }) =>
+    (
+        <div>
+            <button onClick={onClick}>Get all apis</button>
+        </div>
     );
-  }
-
-}
 
 ApisComponent.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  apis: PropTypes.object.isRequired,
+    onClick: PropTypes.func.isRequired,
+    apis: PropTypes.object.isRequired,
 };
 
 export default ApisComponent;
