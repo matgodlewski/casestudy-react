4f29853f81aaee8f37f32cfbeeced96f71c82792
Mateusz Godlewski <thyen00@gmail.com>
2022-07-20 18:43:06 +0200

    Added category param

--- src/case-study/service/rest.service.jsx
+++ src/case-study/service/rest.service.jsx
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 export default function fetchPublicApis() {
-  return axios.get('https://api.publicapis.org/entries')
+  return axios.get('https://api.publicapis.org/entries', {
+    params: {
+      category: 'Development',
+    },
+  })
     .then((response) => response.data)
     .catch((error) => {
+      // eslint-disable-next-line no-console
       console.log(`error:${error}`);
       throw error;
     });
